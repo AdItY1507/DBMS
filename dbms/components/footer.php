7ecb5dc0aa7e839454606925676480cf1e923395
AdItY1507 <adityarajkashyap.211cs205@nitk.edu.in>
2023-11-03 11:09:37 +0530

    DBMS-ecommerce

---
+++ dbms/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Your Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:+1 323 978 6548"><i class="fas fa-phone"></i>Fax</a>
+         <a href="tel:(+91) 9096 123 456"><i class="fas fa-phone"></i>Phone</a>
+         <a href="mailto:nitkcop@nitk.edu.in"><i class="fas fa-envelope"></i>Email</a>
+         <a href="https://www.google.com/maps/place/State+Bank+of+India+SURATHKAL/@13.0083047,74.7930507,18z/data=!4m14!1m7!3m6!1s0x3ba353836f7807e1:0xc3e900d6f4664537!2sSai+Hospitality+services+Night+Canteen!8m2!3d13.0079705!4d74.7961805!16s%2Fg%2F11fd51bncx!3m5!1s0x3ba352106a1483e3:0xa529262812f2229c!8m2!3d13.008952!4d74.794119!16s%2Fg%2F124t40554?entry=ttu"><i class="fas fa-map-marker-alt"></i>Visit us</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <!-- <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a> -->
+         <!-- <a href="#"><i class="fab fa-twitter"></i>Twitter</a> -->
+         <a href="https://instagram.com/nitk.surathkal?igshid=MzRlODBiNWFlZA=="><i class="fab fa-instagram"></i>Instagram</a>
+         <!-- <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a> -->
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>NITK</span> | all rights reserved!</div>
+
+</footer>
